03d086b6485914b6ffbd76acdc1c7f69e535a8ad
Chris Bryant <chrisbryant.jscript@gmail.com>
2022-05-07 01:17:28 -0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'import/prefer-default-export': 0,
+    'import/extensions': 0,
+    'no-new': 0,
+  },
+  globals: {
+    google: 'readonly',
+  },
+};
